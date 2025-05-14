3ea733ce7cf20757d8c262f3bc3c4082ce020e43
Williams DO REGO <williams.dorego@lan.sio.lyceefulbert.fr>
2025-05-14 11:16:54 +0200

    add4

---
+++ tpDecouvert/tp decouvert/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using tp_decouvert;
+
+voiture car_1 = new voiture("jaguar","gris métalique","rihanna",250);
+voiture car_2 = new voiture("mini", "blanc coquille d'oeuf", "cardi", 1350);
+voiture car_3 = new voiture("mercedes", "jaune moutarde", "tyla", 780);
+voiture car_4 = new voiture("lambo", "vert olive", "minaj", 640);
+voiture car_5 = new voiture("twingo", "bleu nuit", "maria", 960);
+
+car_1.AfficherInfos();
+car_2.AfficherInfos();
+car_3.AfficherInfos();
+car_4.AfficherInfos();
+car_5.AfficherInfos();
+
+car_1.Aug_chevaux();
---
+++ tpDecouvert/tp decouvert/voiture.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_decouvert
+{
+    internal class voiture
+    {
+        private string Marque;
+        private string Couleur;
+        private string Matricule;
+        private float Chevaux;
+
+
+        public voiture(string unemarque, string unecouleur,string unmatricule, float deschevaux)
+        {
+            this.Marque = unemarque;
+            this.Couleur = unecouleur;
+            this.Matricule = unmatricule;
+            this.Chevaux = deschevaux;
+        }
+
+        public voiture(string unmatricule)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+       
+        public voiture(string matricule)
+        {
+            this.Matricule = matricule;
+        }
+        
+
+
+        public void setmarque(string unemarque)
+        {
+            this.Marque = unemarque;
+        }
+        public string getmarque()
+        {
+            return this.Marque;
+        }
+
+        
+        public void setcouleur(string unecouleur)
+        {
+            this.Couleur = unecouleur;
+        }
+        public string getcouleur()
+        { 
+            return this.Couleur; 
+        }
+
+        
+        public void setmatricule(string unmatricule)
+        {
+            this.Matricule = unmatricule;
+        }
+        public string getmatricule()
+        {
+            return this.Matricule;
+        }
+
+        public void setchevaux(float deschevaux)
+        {
+            this.Chevaux = deschevaux;
+        }
+        public float getchevaux()
+        {
+            return this.Chevaux;
+
+        }
+
+        public void AfficherInfos()
+        {
+            Console.WriteLine("Marque:" + this.Marque);
+            Console.WriteLine("couleur:" + this.Couleur);
+            Console.WriteLine("Couleur:" + this.Matricule);
+            Console.WriteLine("nombre de chevaux:" + this.Chevaux);
+        }
+
+        public void  Aug_chevaux(int nb_ajouter)
+        {
+            int res = this.Chevaux += nb_ajouter;
+            this.setchevaux(res);
+            return res;
+        }
+
+    }
+}
